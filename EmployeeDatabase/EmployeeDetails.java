2f17669103856b14f79dcdc533d63e9d12ca45d7
= <=>
2020-10-19 23:26:04 +0530

    Adding Employee Details

---
+++ EmployeeDatabase/EmployeeDetails.java
@@ -0,0 +1,19 @@
+package EmployeeDatabase;
+import java.util.Scanner;
+public class EmployeeDetails 
+{
+	public static void main(String args[])
+	{
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Enter the name of Employee");
+		String name = scr.nextLine();
+		System.out.println("Enter the joining year");
+		int yoj = scr.nextInt();
+		System.out.println("Enter the email");
+		String email = scr.nextLine();
+		System.out.println("Enter the contact number");
+		long num = scr.nextLong();
+		System.out.println("Enter the gender");
+		String gender = scr.nextLine();
+	}
+}
---
+++ EmployeeDatabase/SalaryCalculation.java
@@ -0,0 +1,8 @@
+package EmployeeDatabase;
+public class SalaryCalculation extends EmployementInformation
+{
+	public static void main()
+	{
+	float salary = (rate*30) + overtime;
+	}
+}
